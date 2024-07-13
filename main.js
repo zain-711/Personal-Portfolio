c341887b7bd4b8df9eaa380e3badc9bb29fb0611
Zain Ali <zain3002ali@gmail.com>
2024-07-13 16:46:45 +0100

    Title and interactive nav bar added, with basic styling

---
+++ main.js
@@ -0,0 +1,11 @@
+function menuNav(x) {
+  x.classList.toggle("change");
+}
+
+function toggleList() {
+  let list = document.getElementById("nav-links");
+
+  list.classList.toggle("show");
+}
+
+// const list = document.querySelector("#nav-links");
